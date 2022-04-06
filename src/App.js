4521a2d546bc469df684d3e228ddb10beb4d7397
manyfaces-coder <semenovoleg99@yandex.ru>
2022-04-06 18:56:52 +0700

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import {BrowserRouter} from "react-router-dom";
+import {Route, Routes} from "react-router";
+import Room from './pages/Room';
+import Main from './pages/Main';
+import NotFound404 from './pages/NotFound404';
+
+
+
 function App() {
   return (
-    <div>
-    </div>
-  );
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Main />}></Route>
+          <Route exact path="/room/:id" element={<Room />}></Route>
+          <Route path='*' element={<NotFound404 />}></Route>
+        </Routes>
+      </BrowserRouter>
+  ); 
 }
 
 export default App;
---
+++ src/pages/Room/index.js
@@ -0,0 +1,7 @@
+export default function Room() {
+    return(
+        <div>
+            Room
+        </div>
+    );
+}
