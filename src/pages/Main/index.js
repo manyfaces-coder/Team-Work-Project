279e419d1c90206b94cb42ca30003248c9e8f676
whiskey1337 <redbullet0610@gmail.com>
2022-04-18 16:47:56 +0700

    Added actions and implemented the connection/disconnection logic on the client

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,37 @@
+import {useState, useEffect} from "react";
+import ACTIONS from "../../socket/actions";
 import socket from "../../socket";
+import {useNavigate} from "react-router";
+import {v4} from "uuid";
 
 export default function Main() {
+    const history = useNavigate();
+    const [rooms, updateRooms] = useState([]);
+
+    useEffect(() => {
+        socket.on(ACTIONS.SHARE_ROOMS, ({rooms = []} = {}) => {
+            updateRooms(rooms);
+        });
+    });
 
     return(
         <div>
-            Main Page
+            <h1>Available Rooms</h1>
+
+            <ul>
+                {rooms.map(roomID => (
+                    <li key={roomID}>
+                        {roomID}
+                        <button onClick={() => {
+                            history(`/room/${roomID}`);
+                        }}>JOIN ROOM</button>
+                    </li>
+                ))}
+            </ul>
+
+            <button onClick={() => {
+                history(`/room/${v4()}`);
+            }}>Create New Room</button>
         </div>
     );
 }
--- src/pages/Room/index.js
+++ src/pages/Room/index.js
@@ -1,4 +1,10 @@
+import {useParams} from "react-router";
+
 export default function Room() {
+    const {id: roomID} = useParams();
+
+    console.log(roomID);
+
     return(
         <div>
             Room
