c1a8a092c1b055d8ac7436f7273307337dcd95b7
manyfaces-coder <semenovoleg99@yandex.ru>
2022-05-05 10:08:43 +0700

    Сhanged 'class' -> 'className'

--- src/pages/NotFound404.js
+++ src/pages/NotFound404.js
@@ -5,12 +5,12 @@ import '../Styles/ErrorStyle.css';
 export default function NotFound404() {
     return (
 
-            <div class="container text">
-                <div class="error_window">
-                    <p class="text_404">404</p>
-                    <p class="text_error">Страница не найдена</p>
+            <div className="container text">
+                <div className="error_window">
+                    <p className="text_404">404</p>
+                    <p className="text_error">Страница не найдена</p>
                     <MyButton>
-                        <Link class='text' to='/'>На главную</Link>
+                        <Link className='text' to='/'>На главную</Link>
                     </MyButton>
                 </div>
             </div>
