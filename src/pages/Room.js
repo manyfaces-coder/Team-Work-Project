18bc199bd2a030dd9e18b36d86079ce42056a3a1
manyfaces-coder <semenovoleg99@yandex.ru>
2022-05-20 02:59:02 +0700

    Added dynamic video location and chat design

--- src/pages/Room.js
+++ src/pages/Room.js
@@ -9,11 +9,40 @@ import Chat from '../images/chat.png'
 import useWebRTC, {LOCAL_VIDEO} from '../hooks/useWebRTC';
 
 
+function layout(clientsNumber = 1) {
+    const pairs = Array.from({length: clientsNumber})
+      .reduce((acc, next, index, arr) => {
+        if (index % 2 === 0) {
+          acc.push(arr.slice(index, index + 2));
+        }
+  
+        return acc;
+      }, []);
+  
+    const rowsNumber = pairs.length;
+    const height = `${100 / rowsNumber}%`;
+  
+    return pairs.map((row, index, arr) => {
+
+      if (index === arr.length - 1 && row.length === 1) {
+        return [{
+          width: '100%',
+          height,
+        }];
+      }
+  
+      return row.map(() => ({
+        width: '50%',
+        height,
+      }));
+    }).flat();
+  }
 
 
 export default function Room() {
     const { id: roomID } = useParams();
     const {clients, provideMediaRef} = useWebRTC(roomID);
+    const videoLayout = layout(clients.length);
 
     console.log(clients);
     console.log(roomID);
@@ -22,10 +51,12 @@ export default function Room() {
             <div className='main'>
                 <div className='main__left'>
                     <div className='main__videos'>
-                        {clients.map((clientID) => {
+                        {clients.map((clientID, index) => {
                             return (
-                                <div key={clientID} id='video_grid'>
+                                <div key={clientID} style={videoLayout[index]} id='video_grid'>
                                     <video
+                                        width='100%'
+                                        height='100%'
                                         ref={instance => {
                                             provideMediaRef(clientID, instance);
                                         }}
@@ -34,7 +65,6 @@ export default function Room() {
                                         muted={clientID === LOCAL_VIDEO}
                                     />
                                 </div>
-
                             )
                         })}
                         
@@ -71,6 +101,16 @@ export default function Room() {
                 <div className='main__right'>
                     <div className='main__header'>
                         <h6 className='text'>Chat</h6>
+                    </div>
+                    < div className='main__chat_window'>
+                        <ul className='messages'>
+
+                        </ul>
+                    </div>
+                    <div className='main__message_container'>
+                        <input id='chat_message' type='text' placeholder='Type your message here...'>
+                            
+                        </input>
                     </div>
 
                 </div>
